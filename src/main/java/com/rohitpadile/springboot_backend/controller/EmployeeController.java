fa26dbb9599c779f5987154ffb2a9c31d67255e5
Rohit Padile <rohitpadile24@gmail.com>
2024-06-25 12:59:59 +0530

    Created Add Employee Rest Api

--- src/main/java/com/rohitpadile/springboot_backend/controller/EmployeeController.java
+++ src/main/java/com/rohitpadile/springboot_backend/controller/EmployeeController.java
@@ -3,10 +3,7 @@ package com.rohitpadile.springboot_backend.controller;
 import com.rohitpadile.springboot_backend.model.Employee;
 import com.rohitpadile.springboot_backend.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,5 +18,11 @@ public class EmployeeController {
     @GetMapping("/employees")
     public List<Employee> getAllEmployees(){
         return employeeRepository.findAll();
+    }
+
+    //CREATE EMPLOYEE REST API
+    @PostMapping("/employees")
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeRepository.save(employee);
     }
 }
