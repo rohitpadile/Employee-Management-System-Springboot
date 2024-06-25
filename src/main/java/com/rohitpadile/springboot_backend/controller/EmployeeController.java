0b9013da58035d234f0966dfa85a2ac283ad6150
Rohit Padile <rohitpadile24@gmail.com>
2024-06-25 21:45:34 +0530

    Created get employee by id rest api

--- src/main/java/com/rohitpadile/springboot_backend/controller/EmployeeController.java
+++ src/main/java/com/rohitpadile/springboot_backend/controller/EmployeeController.java
@@ -1,8 +1,10 @@
 package com.rohitpadile.springboot_backend.controller;
 
+import com.rohitpadile.springboot_backend.exception.ResourceNotFoundException;
 import com.rohitpadile.springboot_backend.model.Employee;
 import com.rohitpadile.springboot_backend.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,5 +26,14 @@ public class EmployeeController {
     @PostMapping("/add-employee")
     public Employee createEmployee(@RequestBody Employee employee) {
         return employeeRepository.save(employee);
+    }
+
+    //get employee by id rest api
+    @GetMapping("/employees/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id){
+        Employee employee = employeeRepository.findById(id).orElseThrow( () ->
+            new ResourceNotFoundException("Employee Not Exist with id " + id)
+        );
+        return ResponseEntity.ok(employee); //working
     }
 }
