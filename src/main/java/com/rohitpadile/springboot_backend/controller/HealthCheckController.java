b76864dad319ac43c46e749fd097614935d741d3
Rohit Padile <rohitpadile24@gmail.com>
2024-07-13 22:44:40 +0530

    WIP

---
+++ src/main/java/com/rohitpadile/springboot_backend/controller/HealthCheckController.java
@@ -0,0 +1,17 @@
+package com.rohitpadile.springboot_backend.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/")
+public class HealthCheckController {
+    @GetMapping
+    public ResponseEntity<Void> healthCheck() {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
