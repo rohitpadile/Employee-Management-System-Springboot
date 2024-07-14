cfe60c827f1fdbe5b9ec20c4acb9547a0043b9be
Rohit Padile <rohitpadile24@gmail.com>
2024-07-15 00:34:22 +0530

    WIP

--- src/main/java/com/rohitpadile/springboot_backend/controller/EmployeeController.java
+++ src/main/java/com/rohitpadile/springboot_backend/controller/EmployeeController.java
@@ -11,7 +11,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@CrossOrigin(origins ={ "http://192.168.1.6:3000/"})
 @RestController
 @RequestMapping("api/v1/")
 public class EmployeeController {
